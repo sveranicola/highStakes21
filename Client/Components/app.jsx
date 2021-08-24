b7985843a75fed29e8e487990221e3884b29f378
Santiago Vera Nicola <sveranicola12@gmail.com>
2021-08-24 13:37:32 -0400

    init

---
+++ Client/Components/app.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect, useLayoutEffect, useRef } from 'react';
+import Axios from 'axios';
+
+
+var App = () => {
+  return (
+    <div>
+      hello world
+    </div>
+  )
+}
+
+export default App;
